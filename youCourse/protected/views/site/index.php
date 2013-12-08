4e180e99450f351a2935f7c82406fb8d55124e7b
jgarciax <gxjorge@hotmail.com>
2013-12-08 11:15:01 +0100

    welcome

--- youCourse/protected/views/site/index.php
+++ youCourse/protected/views/site/index.php
@@ -23,7 +23,7 @@ $this->pageTitle=Yii::app()->name;
 <div class="right">
     <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
-    <p>Congratulations! You have successfully created your Yii application.</p>
+    <p>Welcome to the Youcours webpage!</p>
 
     <p>You may change the content of this page by modifying the following two files:</p>
     <ul>
