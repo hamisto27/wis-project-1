bafc7c127bde630f84a5becd4d5c9350b9345ab3
auplisni <auplisni@ulb.ac.be>
2013-12-12 21:45:57 +0100

    Mighty Calendar

--- youCourse/protected/views/layouts/main.php
+++ youCourse/protected/views/layouts/main.php
@@ -1,6 +1,21 @@
 <?php /* @var $this Controller */ ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+
+<script>
+    $(document).ready(function() {
+
+        // page is now ready, initialize the calendar...
+
+        $('#calendar').fullCalendar({
+            // put your options and callbacks here
+        })
+
+    });
+</script>
+
+
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="language" content="en" />
@@ -17,6 +32,7 @@
 
     <!-- calendar -->
     <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/fullcalendar.css"/>
+    <script type='text/javascript' src='<?php echo Yii::app()->request->baseUrl; ?>/js/jquery-1.10.2.js'></script>
     <script type='text/javascript' src='<?php echo Yii::app()->request->baseUrl; ?>/js/jquery.js'></script>
     <script type='text/javascript' src='<?php echo Yii::app()->request->baseUrl; ?>/js/fullcalendar.js'></script>
 
@@ -24,6 +40,8 @@
 </head>
 
 <body>
+
+<div id='calendar'></div>
 
 <div class="container" id="page">
 
