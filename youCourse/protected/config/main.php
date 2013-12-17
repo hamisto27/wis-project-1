c4b7e7d8eb019e6cb5810582ee3c3fa6946635bc
auplisni <auplisni@ulb.ac.be>
2013-12-17 09:07:46 +0100

    api

--- youCourse/protected/config/main.php
+++ youCourse/protected/config/main.php
@@ -90,6 +90,9 @@ return array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+		        'rules'=>require(
+                    dirname(__FILE__).'/../extensions/starship/restfullyii/config/routes.php'
+                ),
 			),
 		),
 		*/
